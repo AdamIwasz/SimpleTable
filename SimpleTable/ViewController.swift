093ad50397be3c6159d66f6c04e54c33da2cee0a
AdamIwasz <adam.iwaszkiewiczai@gmail.com>
2022-09-15 21:34:29 +0200

    Initial Commit

---
+++ SimpleTable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleTable
+//
+//  Created by Adam Iwaszkiewicz UCTI on 15/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
